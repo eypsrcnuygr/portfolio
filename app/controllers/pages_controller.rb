70c09edcad04c7f963032f1ed9e7217b76eaf643
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-13 17:32:27 +0300

    Add links and cv download functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
-  def home
+  def home; end
+
+  def download_pdf
+    send_file "#{Rails.root}/public/assets/docs/CV.pdf", type: 'application/pdf', x_sendfile: true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'download_pdf', to: "pages#download_pdf"
 end
