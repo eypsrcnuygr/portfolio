e51c824382d2d6db011afcc1e55203ce9d054bbe
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-14 23:28:44 +0300

    Add form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
         format.html { redirect_to root_path, notice: 'Your message has been sent' }
         format.json   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
-        format.html { render 'home', alert: 'There was something wrong with your message' }
+        format.html { redirect_to root_path, alert: 'There was something wrong with your message' }
         format.json   { flash.now[:error] = @message = "Message did not send." }
       end
     end
