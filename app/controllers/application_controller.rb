9dd1220749de1ec94139299c2e5e970b7054fa78
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-14 22:52:46 +0300

    Working form but not styled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    @contact = Contact.new(params)
+  end
+
+  def create_form
+    @contact = Contact.new(params)
+    @contact.request = request
+    respond_to do |format|
+      if @contact.deliver
+        # re-initialize Contact object for cleared form
+        @contact = Contact.new
+        format.html { redirect_to root_path, notice: 'Your message has been sent' }
+        format.json   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+      else
+        format.html { render 'home', alert: 'There was something wrong with your message' }
+        format.json   { flash.now[:error] = @message = "Message did not send." }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'contact', to: 'pages#create_form'
   root 'pages#home'
   get 'download_pdf', to: 'pages#download_pdf'
 end
