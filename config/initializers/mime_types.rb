ba46370a95ab31a21ac9cb74debfd524abbc7dd3
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-19 17:02:28 +0300

    The last one for the font issue

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,5 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.register 'text/richtext', :woff2
-Mime::Type.register 'text/richtext', :ttf
+Mime::Type.register "woff2", :woff2
+Mime::Type.register "ttf", :ttf
+
