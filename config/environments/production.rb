dd57be5b4cd97a69640d93b68b7f0ad023f909a9
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-14 20:53:48 +0300

    Change the production.rb host name and domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'sercan-portfolio-2.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.eyupsercanuygur.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -118,7 +118,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'sercan-portfolio-2.herokuapp.com',
+    domain: 'https://www.eyupsercanuygur.com/',
     user_name: ENV['GMAIL_EMAIL'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
