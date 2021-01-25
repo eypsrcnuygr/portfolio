7b8f66eccd7b013535d86511c0d5c677c593f8ac
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-25 23:15:17 +0300

    change into react

--- config/initializers/mime_types.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
-
-Mime::Type.register "font/ttf", :ttf, [], %w(ttf)
-Mime::Type.register "font/woff", :woff, [], %w(woff)
-Mime::Type.register "font/woff2", :woff2, [], %w(woff2)
